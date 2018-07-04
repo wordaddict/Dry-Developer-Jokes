0b1eec78f526fe54e543abc0ef719f5281e481c4
wordaddict <madeyinka6@gmail.com>
2018-07-04 15:54:18 +0100

    joke data gotten successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Welcome to Terragon';
+  loader = true;
+  public jokes;
+  constructor(
+    private appService :AppService
+  ){
+    this.delayApi(); 
+  }
+
+  ngOnInit(){
+    this.getJokes();
+
+  }
+
+  delayApi(){
+    setTimeout(() => {
+      this.loader = false;
+    }, 5000);
+  }
+
+  getJokes() {
+    this.appService.getDryJoke()
+      .subscribe((data) => {
+        this.jokes = data;
+        console.log('Jokes', this.jokes);
+      })
+  }
 }
